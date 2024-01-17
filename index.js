c8d8ed31abf5d7683482cc91103f34b60ba66d95
wardaamjad <wardaamjad4@gmail.com>
2024-01-17 22:26:13 +0500

    feat:live changes on backend

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
-
+app.use(cors()); 
 // Connect to MongoDB (replace 'your-database-uri' with your MongoDB connection string)
-mongoose.connect('your-database-uri', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://127.0.0.1:27017/assignment');
 
 // Define MongoDB schema and model
 const universitySchema = new mongoose.Schema({
-  country: String,
-  universities: [
+  university: [
     {
       name: String,
+      country:String,
       domains: [String],
       alpha_two_code: String,
       web_pages: [String],
-      state_province: String,
+      
     },
   ],
 });
@@ -29,8 +29,11 @@ const University = mongoose.model('University', universitySchema);
 // API to save country name and data of universities in the database
 app.post('/api/save', async (req, res) => {
   try {
-    const { country, universities } = req.body;
-    const savedData = await University.create({ country, universities });
+    const { universities} = req.body;
+    universities.map(async (university)=>{
+      await University.create({university});
+
+    })
     res.json(savedData);
   } catch (error) {
     console.error('Error saving data:', error);
@@ -50,6 +53,19 @@ app.get('/api/retrieve/:country', async (req, res) => {
   }
 });
 
+// API to retrieve data of universities based on country name and uni name
+
+
+app.get('/api/retrieveByUni/:country/:name', async (req, res) => {
+  try {
+    const { country , name} = req.params;
+    const data = await University.findOne({'university.name':name, 'university.country':country});
+    res.json(data || {});
+  } catch (error) {
+    console.error('Error retrieving data:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 // API to update the data of a university based on country name and university ID
 app.put('/api/update/:country/:universityId', async (req, res) => {
   try {
